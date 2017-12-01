3838a76d9d2d3a96b0abc6bbc8f6edde15b048a1
Tibin Thomas <tibinthomas@outlook.com>
2017-12-01 12:14:14 +0530

    Create model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StateComponent } from './state/state.component';
+import { DistComponent } from './dist/dist.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StateComponent,
+    DistComponent,
+    CityComponent
   ],
   imports: [
     BrowserModule
