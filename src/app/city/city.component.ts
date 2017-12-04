71746b2d639d1ef8e3f03c86dde14d75186899c0
Tibin Thomas <tibinthomas@outlook.com>
2017-12-04 10:57:04 +0530

    V 1

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -12,10 +12,12 @@ export class CityComponent implements OnInit, OnChanges {
   constructor(private data: Data) { }
 
   cityToDisplay: string[] = [];
+  selectedDistrictIndex: number;
 
   ngOnInit() {
-    this.data.
-    this.cityToDisplay = Object.values(this.data.fakeSDC.city[this.data.selectedDistrictIndex])[0];
+    this.data.selectedDistrictIndex.subscribe((index) => { this.selectedDistrictIndex = index;
+      this.cityToDisplay = Object.values(this.data.fakeSDC.dist[this.selectedDistrictIndex])[0];
+    });
   }
 
   ngOnChanges(changes: SimpleChanges) {
